a66e437544d5d3e2e6d4c6d1f05d629434615f9f
icancode23 <ernipunarora@gmail.com>
2017-04-09 14:00:40 +0530

    added layout for voice interactions

--- app/src/main/java/com/example/nipunarora/finbot/Activities/Bot_Activity.java
+++ app/src/main/java/com/example/nipunarora/finbot/Activities/Bot_Activity.java
@@ -2,18 +2,44 @@ package com.example.nipunarora.finbot.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import com.example.nipunarora.finbot.Fragments.TextInputFragment;
 import com.example.nipunarora.finbot.R;
 
-public class Bot_Activity extends AppCompatActivity {
+public class Bot_Activity extends AppCompatActivity implements View.OnClickListener {
     FrameLayout input;
+    CardView voice_input,text_input;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bot_);
-        input=(FrameLayout)findViewById(R.id.interaction_options);
-        getSupportFragmentManager().beginTransaction().replace(R.id.interaction_options,new TextInputFragment(),"text").commit();
+        voice_input=(CardView)findViewById(R.id.voice);
+        text_input=(CardView)findViewById(R.id.textit);
+        text_input.setOnClickListener(this);
+        voice_input.setOnClickListener(this);
+
+        /*input=(FrameLayout)findViewById(R.id.interaction_options);
+        getSupportFragmentManager().beginTransaction().replace(R.id.interaction_options,new TextInputFragment(),"text").commit();*/
     }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId())
+        {
+            case R.id.voice:
+                Toast.makeText(this,"Voice Button cLicked",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.textit:
+                Toast.makeText(this,"Text Button cLicked",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                ;
+                break;
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/nipunarora/finbot/Activities/Voice_Bot.java
@@ -0,0 +1,15 @@
+package com.example.nipunarora.finbot.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.nipunarora.finbot.R;
+
+public class Voice_Bot extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voice__bot);
+    }
+}
