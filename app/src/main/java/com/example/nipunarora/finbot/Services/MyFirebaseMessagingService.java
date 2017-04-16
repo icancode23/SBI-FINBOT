7ceeb043fcb1d8ef20cc11e771eae36b6bf69313
icancode23 <ernipunarora@gmail.com>
2017-04-17 00:17:26 +0530

    configured Image Notifications

--- app/src/main/java/com/example/nipunarora/finbot/Services/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/nipunarora/finbot/Services/MyFirebaseMessagingService.java
@@ -1,9 +1,13 @@
 package com.example.nipunarora.finbot.Services;
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
@@ -18,6 +22,11 @@ import com.firebase.jobdispatcher.Job;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URL;
+
 import static com.google.android.gms.internal.zzt.TAG;
 
 /**
@@ -31,7 +40,21 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         if(remoteMessage.getData().size() >0)
 
         {
+            String messagebody,imageurl;
             Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+            try
+            {
+                JSONObject data=new JSONObject(remoteMessage.getData());
+                messagebody=data.getString("Message");
+                imageurl=data.getString("image");
+            }
+            catch (Exception e)
+            {
+                Log.d("Json Exception",e.toString());
+                messagebody=null;
+                imageurl=null;
+            }
+            sendBigNotification(messagebody,imageurl);
 
 
 
@@ -43,19 +66,30 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         {
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
-            sendNotification(remoteMessage.getNotification().getBody());
+            sendBigNotification(remoteMessage.getNotification().getBody(),"https://i.imgur.com/59JsFSI.png");
         }
     }
 
-    private void sendNotification(String messageBody) {
+    //************************** CREATING SMALL NOTIFICATIONS *************//
+
+    private void sendSmallNotification(String messageBody) {
         Intent intent = new Intent(this, Bot_Activity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
                 PendingIntent.FLAG_ONE_SHOT);
+        Bitmap image;
+        try {
+            URL url = new URL("http://api.androidhive.info/images/minion.jpg");
+             image = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+        } catch(IOException e) {
+            image=null;
+            Log.d("Bitmap error",e.toString());
+        }
 
         Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.drawable.man)
+                .setLargeIcon(image)
                 .setContentTitle("FinBot")
                 .setContentText(messageBody)
                 .setAutoCancel(true)
@@ -68,5 +102,44 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
     }
+
+
+    //**************** CREATING THE BIG NOTIFICATION *********************??
+
+    private void sendBigNotification(String messageBody,String imgurl) {
+        Intent intent = new Intent(this, Bot_Activity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+        Bitmap image;
+        try {
+            URL url = new URL(imgurl);
+            image = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+        } catch(IOException e) {
+            image=null;
+            Log.d("Bitmap error",e.toString());
+        }
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Bitmap Icon = BitmapFactory.decodeResource(getResources(), R.drawable.businessmanicon);
+        Notification notification = new Notification.Builder(getApplicationContext())
+                .setContentTitle("FinBot")
+                .setContentText(messageBody)
+                .setSmallIcon(R.drawable.man)
+                .setLargeIcon(Icon)
+                .setPriority(Notification.PRIORITY_MAX)
+                .setStyle(new Notification.BigPictureStyle()
+                        .bigPicture(image)
+                        .setBigContentTitle("Finbot")
+                .setSummaryText(messageBody))
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent)
+                .build();
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notification);
+    }
+
 
 }
