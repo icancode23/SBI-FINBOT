374dc91356151eaeea0af42c73a7cfe129840450
icancode23 <ernipunarora@gmail.com>
2017-04-18 14:27:06 +0530

    changes in token

--- FinBotNotifications/ForexDataMessage.py
+++ FinBotNotifications/ForexDataMessage.py
@@ -2,7 +2,7 @@ import requests
 import json
 head={'project_id': 'finbot-2ad8f', 'Content-Type': 'application/json', 'Authorization': 'key=AAAA7CQoP28:APA91bGLUFoa-alAOK7DDA3lnQwL1yKwChX8WulEZSgmxsfPBb4o0L-CybLLgt2J1jQ2I8FI4uuv1k7IALTPtZzYc68DZz7pjmYxxmu6_qKRbDeJj38oX7rU7qiPln8wGGKge1dclnyg'}
 # messagestring=raw_input("Enter the message you wish to send as notification:")
-d={'to': "cipc_dfrfls:APA91bGJctu_Xfbs_69dENfaj8kWagPMGVlqM8knSQpg-XXd0bpklEnnlEbDptifR95wnj3iQau6PLt5XtYJLurgmoZtCT8iNYCRu4RukZYCfvZXtMpIKEFCIQ-_f2OeLxeDIU7iTkfP"
+d={'to': "cfrIlEtRAJ4:APA91bEU5jOkL9EPpukloYF2sSlAVZ9tse-SH4wUWV8vvGNfSXPJOx8s5uyiqNohYxcyDZabLYqrhWUYjP_avAS-1NHiQVPCuNMIRwLr6wB8h9m7juMfGTe8TbrNxsmXzAXvloPWbRX_"
 , 'priority': 10,
 	"data" : {
       "Message" : "Going to US...get your dollars ",
--- FinBotNotifications/wallettransact.py
+++ FinBotNotifications/wallettransact.py
@@ -2,7 +2,7 @@ import requests
 import json
 head={'project_id': 'finbot-2ad8f', 'Content-Type': 'application/json', 'Authorization': 'key=AAAA7CQoP28:APA91bGLUFoa-alAOK7DDA3lnQwL1yKwChX8WulEZSgmxsfPBb4o0L-CybLLgt2J1jQ2I8FI4uuv1k7IALTPtZzYc68DZz7pjmYxxmu6_qKRbDeJj38oX7rU7qiPln8wGGKge1dclnyg'}
 # messagestring=raw_input("Enter the message you wish to send as notification:")
-d={'notification': {'sound': 'default', 'body': 'You have withdrawn Rs 5000',"title":"FinBot",'picture':'http://api.androidhive.info/images/minion.jpg',}, 'to': "cipc_dfrfls:APA91bGJctu_Xfbs_69dENfaj8kWagPMGVlqM8knSQpg-XXd0bpklEnnlEbDptifR95wnj3iQau6PLt5XtYJLurgmoZtCT8iNYCRu4RukZYCfvZXtMpIKEFCIQ-_f2OeLxeDIU7iTkfP"
+d={'notification': {'sound': 'default', 'body': 'You have withdrawn Rs 5000',"title":"FinBot",'picture':'http://api.androidhive.info/images/minion.jpg',}, 'to': "cfrIlEtRAJ4:APA91bEU5jOkL9EPpukloYF2sSlAVZ9tse-SH4wUWV8vvGNfSXPJOx8s5uyiqNohYxcyDZabLYqrhWUYjP_avAS-1NHiQVPCuNMIRwLr6wB8h9m7juMfGTe8TbrNxsmXzAXvloPWbRX_"
 , 'priority': 10,}
 lucky=requests.post(url='https://fcm.googleapis.com/fcm/send',headers=head,data=json.dumps(d))
 print lucky.text
